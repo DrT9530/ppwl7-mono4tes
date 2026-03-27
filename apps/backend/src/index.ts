18cff766330f22bc40c4cd6fa4faa00a6eba89e0
DrT. <athallahrisky@gmail.com>
2026-03-27 19:32:45 +0700

    coba coba

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -104,7 +104,8 @@ const app = new Elysia()
     // Set cookie session
     session.value = sessionId;
     session.maxAge = 60 * 60 * 24; // 1 hari
-
+    session.secure = true; 
+    session.sameSite = "none";
     // !!! ubah url frontend jadi dynamic ambil dari env
     return redirect(`${process.env.FRONTEND_URL}/classroom`);
   })
